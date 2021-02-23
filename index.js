438981092e4d016edc91b935d609db5a926e02e7
phamtanphat <phatdroid94@gmail.com>
2021-02-23 21:17:07 +0700

    data trong body

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
  
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
 app.get('/cong/:a/:b', function (request, response) {
     const {a , b} = request.params
     if(isNaN(a) || isNaN(b)){
       return response.send({success : false , message : "Giá trị không phải là số"}).end();
     }
     return response.send({success : true , result : parseInt(a) + parseInt(b)}).end()
+})
+
+app.post("/tru" , function (req , res) {
+    console.log(req.body)
 })
  
 app.listen(3000)
